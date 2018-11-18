ede009e4f1195e4f40d541b6c912767ad7df2376
Sjoerd van Staveren <sjoerd@van-staveren.net>
2018-11-18 16:06:18 +0100

    PopUp window form button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+const BrowserWindow = require('electron').remote.BrowserWindow;
+const path = require('path');
+const url = require('url');
 
+console.log(document.getElementById('CreateWindowBtn'))
 
+const CreateWindowBtn = document.getElementById('CreateWindowBtn');
+
+CreateWindowBtn.addEventListener('click', function(event){
+    let PopUpWindow = new BrowserWindow({
+        width: 400,
+        height: 200
+    });
+
+    PopUpWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'popup.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+
+    
+})
